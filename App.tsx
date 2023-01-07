94e3d6eef203dd4df1d3256c196bd4823d00b92c
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-01-07 13:05:22 -0300

    feat: :sparkles: add styled components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-// import { Login } from "./src/screens/Login";
+import { StatusBar } from "expo-status-bar";
+
+import { ThemeProvider } from "styled-components";
+
 import { Login } from "@/screens/Login";
+
+import theme from "@/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Login />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colors.black,
     alignItems: "center",
     justifyContent: "center",
   },
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export default {
+  colors: {
+    black: '#181818',
+    white: '#f2f2f2',
+  }
+}
